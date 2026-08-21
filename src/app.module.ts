8763a6add959cf9511af13ed2e6e5b0760e38f81
namin <tnddl032@gmail.com>
2026-08-21 14:42:57 +0900

    test(e2e): e2e 환경변수 검증 실패 수정 및 .env.test 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { envValidationSchema } from './config/env.validation';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
       load: [appConfig, databaseConfig, redisConfig],
       validationSchema: envValidationSchema,
       validationOptions: { abortEarly: false },
--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -2,7 +2,7 @@ import * as Joi from 'joi';
 
 export const envValidationSchema = Joi.object({
   NODE_ENV: Joi.string()
-    .valid('development', 'production')
+    .valid('development', 'test', 'production')
     .default('development'),
 
   PORT: Joi.number().port().default(3000),
